0f4c1a9dd47546d0e90d903766366fe1e83d9535
Guy Molinari <guy.molinari@disney.com>
2021-03-03 08:55:14 -0800

    parent cd772f7c207c13486ca1af2fdbe1a3c3d99c6d08 author Guy Molinari <guy.molinari@disney.com> 1614790514 -0800 committer Guy Molinari <molinariguy@gmail.com> 1618154437 -0700
    Initial release.
    
    Added deployment stuff.
    
    Update architecture diagram.
    
    Update architecture diagram.
    
    Added example stuff.
    
    Added client code.
    
    Add core library.
    
    Added custom functions.
    
    Added parquet utility.
    
    Added Kafka streaming loader.
    
    Added parquet loader.
    
    Added data node launcher.
    
    Added proxy.
    
    Added server package.
    
    Added shared code.
    
    Added output sink.
    
    Added data source and SQL translator code.
    
    Added integration tests.
    
    Add test data.
    
    Change city ranking to IntDirect mapper.
    
    Remove extraneous stuff.
    
    Minor documentation updates.
    
    Recompile GRPC.
    
    update tests for use with quanta
    
    Apply various fixes for integration test suite.
    
    Updated tests for use with cities and city-zip table
    
    Fix remaining tests
    
    Added Curtis Deems as a contributor.
    
    Scrub disney internal info.
    
    Fix remaining tests
    
    Added Curtis Deems as a contributor.
    
    Scrub disney internal info.

---
+++ core/connection_test.go
@@ -0,0 +1,34 @@
+package core
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	_ "github.com/stretchr/testify/require"
+)
+
+func TestCreateConnection(t *testing.T) {
+	os.RemoveAll("./testdata/metadata/cities")
+	c, err := OpenConnection("./testdata", "./testdata/metadata", "cities", false, 0, 0, nil)
+	assert.Nil(t, err)
+	assert.NotNil(t, c)
+	assert.NotNil(t, c.TableBuffers)
+	assert.Equal(t, len(c.TableBuffers), 1)
+	assert.NotNil(t, c.TableBuffers["cities"])
+}
+
+// func TestCreateRecursiveConnection(t *testing.T) {
+// 	os.RemoveAll("./testdata/metadata/user")
+// 	os.RemoveAll("./testdata/metadata/events")
+// 	os.RemoveAll("./testdata/metadata/ab_test")
+// 	os.RemoveAll("./testdata/metadata/dss_id")
+// 	os.RemoveAll("./testdata/metadata/guest_id")
+// 	os.RemoveAll("./testdata/metadata/anonymous_id")
+// 	os.RemoveAll("./testdata/metadata/session_id")
+// 	os.RemoveAll("./testdata/metadata/subscription_id")
+// 	c, err := OpenConnection("./testdata", "./testdata/metadata", "user", true, 0, 0, nil)
+// 	assert.Nil(t, err)
+// 	assert.NotNil(t, c)
+// 	assert.Equal(t, len(c.TableBuffers), 6)
+// }
